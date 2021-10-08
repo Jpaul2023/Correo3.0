9128b578869192143dda61134e3eeca7ba4da1cb
jjose <jjose@LAPTOP-U8KIGCV1>
2021-10-08 16:22:54 -0600

    Probando error de github1

--- Correo/Recibidos.aspx.designer.cs
+++ Correo/Recibidos.aspx.designer.cs
@@ -7,9 +7,20 @@
 // </generado automáticamente>
 //------------------------------------------------------------------------------
 
-namespace Correo {
-    
-    
-    public partial class Recibidos {
+namespace Correo
+{
+
+
+    public partial class Recibidos
+    {
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
