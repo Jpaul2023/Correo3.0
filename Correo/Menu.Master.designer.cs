547ec98a37debe073d453049f0f00bbc09684197
cesar merida <juegostrios@hotmail.com>
2021-10-13 16:06:51 -0600

    sd

--- Correo/Menu.Master.designer.cs
+++ Correo/Menu.Master.designer.cs
@@ -42,19 +42,12 @@ namespace Correo
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
 
         /// <summary>
-<<<<<<< HEAD
-        /// Control Label1.
-=======
         /// Control TextBoxdestinatario.
->>>>>>> 66e7f4d7ae6c7037494799768b0ed468442e0e15
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-<<<<<<< HEAD
-        protected global::System.Web.UI.WebControls.Label Label1;
-=======
         protected global::System.Web.UI.WebControls.TextBox TextBoxdestinatario;
 
         /// <summary>
@@ -74,7 +67,6 @@ namespace Correo
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBoxmensaje;
->>>>>>> 66e7f4d7ae6c7037494799768b0ed468442e0e15
 
         /// <summary>
         /// Control ButtonEnviar.
@@ -83,11 +75,7 @@ namespace Correo
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-<<<<<<< HEAD
-        protected global::System.Web.UI.WebControls.Image Image1;
-=======
         protected global::System.Web.UI.WebControls.Button ButtonEnviar;
->>>>>>> 66e7f4d7ae6c7037494799768b0ed468442e0e15
 
         /// <summary>
         /// Control ContentPlaceHolder1.
